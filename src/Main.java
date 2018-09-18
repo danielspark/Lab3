91c198324ac332cdc755b96223152c310c13a0e1
danielspark <danielp7@illinois.edu>
2018-09-18 12:33:57 -0500

    Done Commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,40 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class Main {
 
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+    }
+
     public static void main(String[] args) {
-        System.out.println("here");
-	// write your code here
+        System.out.println(urlToString("https://www.bls.gov/tus/charts/chart9.txt"));
+        String str = urlToString("https://www.bls.gov/tus/charts/chart9.txt");
+        int count = 0;
+        String find = "Data";
+        String[] splited = str.split("\\s+");
+        for (int i = 0; i < splited.length; i++) {
+            if (splited[i].equalsIgnoreCase(find)) {
+                count++;
+            }
+        }
+        System.out.println(count);
     }
 }
+
+
